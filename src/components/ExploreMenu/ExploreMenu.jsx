72727613564641863a637d53e6d300bd686fc555
Vivek kumar <vivekkumar200377@gmail.com>
2024-06-23 15:52:50 +0530

    Feature: created categories

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import './ExploreMenu.css'
+import { menu_list } from '../../assets/frontend_assets/assets'
 
-const ExploreMenu = () => {
+const ExploreMenu = ({category,setCategory}) => {
   return (
-    <div className='exploreMenu'>
-
+    <div className='exploreMenu' id='exploreMenu' >
+     <h1>Explore our menu</h1>
+     <p className='exploreMenuText'>
+     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+     </p>
+     <div className='exploreMenuList'>
+    {
+      menu_list.map((item,index)=>{
+        return (
+          <div key={index} className='exploreMenuListItem'>
+            <img src={item.menu_image} alt={item.menu_image}  />
+            <p>{item.menu_name}</p>
+          </div>
+        )
+      })
+    }
+     </div>
+     <hr/>
     </div>
   )
 }
