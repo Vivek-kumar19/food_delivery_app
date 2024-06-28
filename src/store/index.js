f59308ceeccd9a85bc53d3af2af8fc35a3782fe7
Vivek kumar <vivekkumar200377@gmail.com>
2024-06-28 20:16:15 +0530

    start again on redux toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,9 @@
 import {configureStore} from '@reduxjs/toolkit' ;
-import menuReducer from './reducers/menuReducer';
+import menuSlice from './reducers/menuSlice';
 
 const store = configureStore({
   reducer: {
-    menuList: menuReducer
+    menuList: menuSlice
   }
 });
 
